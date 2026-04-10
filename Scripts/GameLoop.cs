87402dd668f04ca975b5ee08e4c43286131eaf45
AnnickS <annick.stefanin@gmail.com>
2026-04-10 12:29:19 -0500

    Added the ability to pause
    Added a new event to bring up the in-game menu

--- Scripts/GameLoop.cs
+++ Scripts/GameLoop.cs
@@ -3,14 +3,37 @@ using System;
 
 public partial class GameLoop : Node
 {
+	private bool paused = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Input.MouseMode = Input.MouseModeEnum.Captured;
+		SetMouse();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void SetMouse()
+	{
+		if (paused)
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
+		else
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+	}
+
+    public override void _Input(InputEvent @event)
+    {
+		if(@event.IsActionReleased("Toggle_In-Game_Menu"))
+		{
+			paused = !paused;
+			GetTree().Paused = paused;
+			SetMouse();
+		}
 	}
 }
